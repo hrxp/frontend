a24e03dc3c1f4dd5e839d9847273bec098cff702
Kyle Shockey <shockey@users.noreply.github.com>
2019-08-22 21:11:47 -0700

    add "Sign In with Slack" button

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,10 +2,13 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div>
-      <header>
-        <h1>Login Page</h1>
-      </header>
+    <div className={`login ${props.loggedIn ? 'login--slide' : ''}`}>
+      <div className="login__container">
+        <h1 className="login__header">Welcome to HRX</h1>
+        <a href="https://hackreactorx.slack.com/oauth/authorize?scope=identity.basic%20identity.avatar%20identity.email%20identity.team&client_id=671720357619.724476729541">
+          <img src="https://api.slack.com/img/sign_in_with_slack.png" />
+        </a>
+      </div>
     </div>
   );
 };
