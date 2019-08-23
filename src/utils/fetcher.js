ca8bbe66919f881978389c40a3fe8b3f6b4f9ba6
Kyle Shockey <shockey@users.noreply.github.com>
2019-08-22 21:12:27 -0700

    add Authorization header request interceptor

--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
+axios.interceptors.request.use(function (config) {
+  config.headers.common.Authorization = `Bearer ${localStorage.getItem("hrxp_jwt")}`
+  return config
+});
+
 axios.interceptors.response.use(null, function (error) {
   if(error.response.status === 401) {
     // API auth error
