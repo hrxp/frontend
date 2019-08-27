c510a0c2a83e1f54296c94c154a6585966566d3b
KweonMo Koo <mikekoo2010@gmail.com>
2019-08-27 01:31:13 -0700

    Create search bar component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,30 +1,64 @@
 import React from 'react';
 import axios from 'axios';
+import SearchBox from './SearchBox.js';
 
-export default class Search extends React.Component {
+class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      input: ''
+      input: '',
+      channelID: props.channelID,
+      menu: 'Messages',
+      openSearchBox: false,
+      searched: [],
+      hasMore: true
     }
+
+    this.handleSearch = this.handleSearch.bind(this);
   }
 
-  handleChange(e) {
+  handleSearch(e) {
     let word = e.target.value;
-    this.setState({ input: word });
-    
+    axios
+      .get(`/search`, {
+        params: {
+          word: word,
+        }
+      })
+      .then(res => {
+        this.setState({
+          searched: [...this.state.searched, ...res.data],
+          input: word
+        })
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }
+
+  openSearchBox() {
+    this.setState({ openSearchBox: true })
   }
 
   render() {
+    let searchBox;
+    if (this.state.openSearchBox) {
+      searchBox = <SearchBox 
+                    handleSearch={this.handleSearch}
+                    menu={this.state.menu}
+                    hasMore={this.state.hasMore}
+                    searched={this.state.searched}
+                  />
+    } else {
+      searchBox = '';
+    }
     return (
       <div className="searchbar">
-        <input
-          type="text"
-          className="searchbarinput"
-          placeholder="search..."
-          onChange={this.handleChange}
-        />
+        <button id="searchbutton" onClick={this.openSearchBox}/>
+        {searchBox}
       </div>
     )
   }
 }
+
+export default Search;
