2cfc03a41128f55898b056cfa7a51d0372526771
Michael Frenkel <michael.frenkel@outlook.com>
2019-08-27 17:15:24 -0700

    init

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Messages from "../Messages/Messages";
+import Search from "../Search/Search"
 
 const Chat = ({ channel }) => {
   return (
-    <div>
-      <Messages channel={channel} />
-    </div>
+    <Fragment>
+      <div>
+        <Search style={{ border: 'red 1 solid' }} />
+      </div>
+      <div>
+        <Messages channel={channel} />
+      </div>
+    </Fragment>
   );
 };
 
