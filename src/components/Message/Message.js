f7eda0b8e1ed1e40feea54d354cdc59e0c971ab1
Kyle Shockey <shockey@users.noreply.github.com>
2019-08-29 11:40:17 -0700

    display friendly timestamps

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { fromUnixTime, format } from "date-fns";
 // Note: do we want to display realName? or at least store it? Also, might have to refactor component to a class to handle user obj data states
 
 const Message = ({ message }) => {
@@ -6,13 +7,17 @@ const Message = ({ message }) => {
   const { ts, text } = message;
   return (
     <React.Fragment>
-      <img src={profilePhoto} 
-         className="message__photo"
-         alt="profile picture"
+      <img
+        src={profilePhoto}
+        className="message__photo"
+        alt="profile picture"
       />
       <div className="message__info">
         <div className="message__info__user">{displayName} </div>
-        <div className="message__info__time">{ts}</div>
+        <div className="message__info__time">
+          {/* TODO: localize timestamps into the user's timezone */}
+          {format(fromUnixTime(ts), "MMMM d yyyy HH:mm:ss")}
+        </div>
       </div>
       <div className="message__text">{text}</div>
     </React.Fragment>
