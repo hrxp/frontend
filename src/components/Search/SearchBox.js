4633fbfc3284259dfd495943c3e8edc3c6d99804
KweonMo Koo <mikekoo2010@gmail.com>
2019-08-29 02:42:29 -0700

    Render search results

--- src/components/Search/SearchBox.js
+++ src/components/Search/SearchBox.js
@@ -1,19 +1,56 @@
 import React from 'react';
+import SearchResults from './SearchResults';
+import Message from '../Message/Message'
 // import InfiniteScroll from 'react-infinite-scroller';
 
-const SearchBox = (props) => {
-  let loader = <div className="loader" key={0}>Loading...</div>
+var resultsStyle = {
+  height: 'auto',
+  position: 'relative',
+  ':hover': {
+    background: 'white',
+    width: '500px',
+    height: '600px'
+  }
+}
 
+// const messageStyle = {
+//   zIndex: 10, 
+//   position: 'relative',
+//   background: 'white',
+//   width: '500px'
+// }
+
+const SearchBox = (props) => {
+  // let loader = <div className="loader" key={0}>Loading...</div>
+  console.log("TEST", props.searched)
   return (
-    <div className="searchbox">
+    <div id="searchbox">
       <div className="searchbar">
-        <input onChange={props.handleChange} id="search" className="searchinput" type="text" />
-        <button onClick={props.handleSearch} >Submit</button>
+        <input 
+          onChange={props.handleChange} 
+          id="search" 
+          className="searchinput" 
+          type="text" 
+        />
+        <button onClick={props.handleSearch} >Search</button>
       </div>
       <div className="searchmenu">
-        {props.menu}
+        {/* {props.menu} */}
       </div>
-      <div className="searchresults">
+      <div 
+        id="searchresults" 
+        style={resultsStyle}
+      >
+        {props.searched.map((msg, i) => {
+          if (props.open) {
+            return (
+              <SearchResults msg={msg} key={i} />
+              // <Message message={msg} style={messageStyle}/>
+            )
+          } else {
+            return('')
+          }
+        })}
         {/* <InfiniteScroll
           pageStart={0}
           loadMore={props.handleSearch}
