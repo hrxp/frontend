2e046ac5590112e3ceb6663dc4efd8b718baff31
Kyle Shockey <shockey@users.noreply.github.com>
2019-08-29 13:39:50 -0700

    add loading indicator to Messages

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -7,26 +7,23 @@ class Chat extends Component {
     this.state = {
       headerExpanded: false,
     };
-    }
-
-  componentDidMount() {
-    fetchChannelMessages(this.props.currentChannel.name).then(messages =>
-      this.setState({ messages: messages })
-    );
   }
 
   expandHeaderTopic() {
     if (this.props.currentChannel.topic.length > 60) {
       this.setState({
-        headerExpanded: !this.state.headerExpanded
-      })
+        headerExpanded: !this.state.headerExpanded,
+      });
     }
   }
 
   render() {
-    let topicDisplay = '';
-    if (this.state.headerExpanded === false && this.props.currentChannel.topic.length > 60) {
-      topicDisplay = this.props.currentChannel.topic.slice(0, 60) + '...'
+    let topicDisplay = "";
+    if (
+      this.state.headerExpanded === false &&
+      this.props.currentChannel.topic.length > 60
+    ) {
+      topicDisplay = this.props.currentChannel.topic.slice(0, 60) + "...";
     } else {
       topicDisplay = this.props.currentChannel.topic;
     }
@@ -36,15 +33,15 @@ class Chat extends Component {
         <div className="chat__header">
           <div className="chat__header__container">
             <div className="chat__header__info">
-            <div className="chat__header__title">
-              #{this.props.currentChannel.name}
-            </div>
-            <p
-              onClick={this.expandHeaderTopic.bind(this)}
-              className="chat__header__topic"
-            >
-              {topicDisplay}
-            </p>
+              <div className="chat__header__title">
+                #{this.props.currentChannel.name}
+              </div>
+              <p
+                onClick={this.expandHeaderTopic.bind(this)}
+                className="chat__header__topic"
+              >
+                {topicDisplay}
+              </p>
             </div>
             <div className="chat__header__logo">
               <img
@@ -52,11 +49,11 @@ class Chat extends Component {
                 src="https://static1.squarespace.com/static/ta/522a22cbe4b04681b0bff826/3242/assets/images/logo/HR_at_Galvanize_Stacked.png"
                 alt="Hack Reactor logo"
               />
-          </div>
+            </div>
           </div>
         </div>
-          <Messages currentChannel={this.props.currentChannel} />
-        </div>
+        <Messages currentChannel={this.props.currentChannel} />
+      </div>
     );
   }
 }
