e75334bc0fad682e073204c0e34e262dd380163b
KweonMo Koo <mikekoo2010@gmail.com>
2019-08-29 02:42:29 -0700

    Render search results

---
+++ src/components/Search/SearchResults.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const SearchResults = (props) => {
+  const { displayName, profilePhoto } = props.msg.createdBy;
+  const { ts, text } = props.msg;
+  return (
+    <div className='searchresults' 
+      style={{ 
+        zIndex: 10, 
+        position: 'relative',
+        background: 'white',
+        width: '500px'
+      }}>
+        <img src={profilePhoto} 
+          className="message__photo"
+          alt="pfp"
+        />
+        <div className="message__info">
+          <div className="message__info__user">{displayName} </div>
+          <div className="message__info__time">{ts}</div>
+        </div>
+        <div className="message__text">Msg: {text}</div>
+      
+    </div>
+  )
+}
+
+export default SearchResults;
